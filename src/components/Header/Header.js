7d4e0db7c1e2584103d90db3f40bfbd204d2047b
arshiha bristy <arshihabristy5@gmail.com>
2022-03-25 22:51:35 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="head">
+    <div className="header">
       <h1>The Bag Bazzar</h1>
+      <h4>Selected 3 Bags</h4>
     </div>
   );
 };
