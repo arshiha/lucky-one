bd3bb57ad31a87efada746a8814de50e1b8e3aef
arshiha bristy <arshihabristy5@gmail.com>
2022-03-26 12:29:22 +0600

    btn added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
   const { name, img, price } = props.product;
   return (
-    <div>
+    <div className="product">
       <img src={img} alt=""></img>
+      <div className="product-info">
+        <p className="product-name">Nmae: {name}</p>
+        <p>Price: {price}</p>
+      </div>
+      <button className="btn">
+        <p>Add to Cart</p>
+      </button>
     </div>
   );
 };
